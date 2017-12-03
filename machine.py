985c4dad2e3f2c35d64d3086ede87467a7d98376
Charles Arnaudo <charlesarnaudo@gmail.com>
2017-12-03 15:52:53 -0500

    restarting

--- machine.py
+++ machine.py
@@ -1,13 +1,13 @@
 import sys
 import math
-from collections import deque
+import string
+import pprint
 
 
 class EnigmaMachine:
     """
     """
     def __init__(self):
-
         # Create three rotors
         self.rotor0 = 'EKMFLGDQVZNTOWYHXUSPAIBRCJ'
         self.rotor0 = list(self.rotor0)
@@ -15,25 +15,43 @@ class EnigmaMachine:
         self.rotor1 = list(self.rotor1)
         self.rotor2 = 'BDFHJLCPRTXVZNYEIWGAKMUSQO'
         self.rotor2 = list(self.rotor2)
-        # Group rotors together]
+        # Group rotors together
         self.rotors = []
         self.rotors += self.rotor0, self.rotor1, self.rotor2
         # Copy of initial rotor config
-        self.rotors_copy = self.rotors
-    
-    def rotate(self, l, n):
-        return l[-n:] + l[:-n]
-    
-    def encode(self, plaintext):
-        plaintext = plaintext.upper()
-        """Run input through rotors"""
-        print(plaintext)
-        self.rotor0 = self.rotate(self.rotor0, 1)
-        print(self.rotor0)
-            
-   
+        self.rotors_copy = []
+        self.rotors_copy += self.rotor0, self.rotor1, self.rotor2
 
-    def print_current_config(self):
-        """Print current configuration of machine"""
+        # Create alphabet array, for index
+        self.alphabet = list(string.ascii_uppercase)
+    
+    def rotate(self, l):
+        """Rotate rotor right by one"""
+        return l[-1:] + l[:-1]
+
+    def reset(self):
+        """Reset machine to inital config"""
+        self.rotors = self.rotors_copy
+        print("Reset")
         print(self.rotors)
-        print(self.rotors[0].index('K'))
+        print("\n")
+    
+    def encrypt(self, plaintext):
+        """Run input through rotors, and encode"""
+        plaintext = plaintext.upper()
+        ciphertext = ""
+        tempcipher = ""
+        for char in plaintext:
+            index = self.alphabet.index(char)
+            self.rotate(self.rotors[0])
+            for rotor in self.rotors:
+                tempcipher = rotor[index]
+                index = self.alphabet.index(tempcipher)
+            ciphertext += tempcipher
+        return(ciphertext)
+
+    def print_current_state(self):
+        """Print current configuration of machine"""
+        print("Current configuration of rotors")
+        print(self.rotors)
+        print("\n")
--- main.py
+++ main.py
@@ -2,5 +2,14 @@ import machine
 
 if __name__ == '__main__':
     enigma = machine.EnigmaMachine()
+    print("\n")
+    enigma.print_current_state()
     plaintext = input("Enter text to encode\n")
-    enigma.encode(plaintext)
+    ciphertext = enigma.encrypt(plaintext)
+    print("Cipher: " + ciphertext + "\n")
+   
+    enigma.print_current_state()
+    enigma.reset()
+   
+    decrypt = enigma.encrypt(ciphertext)
+    print("Decrypted: " + decrypt)
